eaf42fed2ec387f0c2343512d247626b779bcd0a
koplenov <koplenov@gmail.com>
2024-02-05 19:46:24 +0000

    Deploying to gh-pages from @ koplenov/utils@050475f14234bb3e676b2981ba3d1098d9dd5b8c 🚀

--- web.js
+++ web.js
@@ -6498,6 +6498,7 @@ var $;
                 return `
 				Кол-во символов ${this.Input_text_in_counter_block().value().length}
 			 	Кол-во строк ${this.Input_text_in_counter_block().value().split("\n").length}
+			 	Кол-во окенов (делим слова на пробелы) ${this.Input_text_in_counter_block().value().split(" ").length}
 			`;
             }
         }
