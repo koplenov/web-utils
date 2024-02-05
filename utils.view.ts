050475f14234bb3e676b2981ba3d1098d9dd5b8c
koplenov <koplenov@gmail.com>
2024-02-05 22:44:41 +0300

    added whitespace out

--- utils.view.ts
+++ utils.view.ts
@@ -5,6 +5,7 @@ namespace $.$$ {
 			return `
 				Кол-во символов ${this.Input_text_in_counter_block().value().length}
 			 	Кол-во строк ${this.Input_text_in_counter_block().value().split("\n").length}
+			 	Кол-во окенов (делим слова на пробелы) ${this.Input_text_in_counter_block().value().split(" ").length}
 			`
 		}
 		
