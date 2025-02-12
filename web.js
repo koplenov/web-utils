e73d0ad0892222f333c4d478c3509d8ed7467cfb
koplenov <deploy-bot>
2025-02-12 07:11:10 +0000

    Deploying to gh-pages from @ koplenov/utils@674fd5bb2c42a89efc91c33f5a7e65575abdb0cf 🚀

--- web.js
+++ web.js
@@ -6314,7 +6314,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                const el = next.target;
+                const el = this.dom_node();
                 const from = el.selectionStart;
                 const to = el.selectionEnd;
                 try {
